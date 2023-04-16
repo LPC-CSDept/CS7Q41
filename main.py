5d5930aa383e87a3985ff59adceed5a95e2a17d4
Kyu Lee <kwooler@gmail.com>
2023-04-15 17:18:45 -0700

    Q41

--- main.py
+++ main.py
@@ -12,19 +12,6 @@ def main():
     ##################################################
     # Code your program here
     ##################################################
-    prev = 0
-    on = 0
-    for v in number:
-        if prev and not v % 2 and not on:
-            on = 1
-            evencnt += 1
-        if v % 2:
-            prev = 0
-            on = 0
-        else:
-            prev = 1
-
-    print(evencnt)
 
 ##
 
